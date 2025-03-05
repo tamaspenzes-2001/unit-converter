02d0d032227864195a0f07d0a075d7f4efc76204
tamaspenzes-2001 <penzestamas@proton.me>
2025-03-05 17:11:22 +0100

    implement user interface

--- unitconverter/src/unitconverter/UnitConverter.java
+++ unitconverter/src/unitconverter/UnitConverter.java
@@ -1,24 +1,30 @@
 package unitconverter;
 
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.Set;
 
 public class UnitConverter{
 	
-	private Map<String, Integer> units;
+	private Map<String, Long> units;
 
 	public UnitConverter(String[] units){
-		this.units = new HashMap<>();
-		int multiplier = 1;
+		this.units = new LinkedHashMap<>();
+		long multiplier = 1;
 		for (String unit: units) {
 			String[] unitArr = unit.split(":");
-			multiplier *= Integer.parseInt(unitArr[1]);
+			multiplier *= Long.parseLong(unitArr[1]);
 			this.units.put(unitArr[0], multiplier);
 		}
 	}
 	
-	public float convert(int amount, String fromUnit, String toUnit) {
-		return amount / (float) units.get(fromUnit) * units.get(toUnit);
+	public double convert(double amount, String fromUnit, String toUnit) {
+		return amount / (double) units.get(fromUnit) * units.get(toUnit);
+	}
+	
+	public String[] getUnits() {
+		Set<String> keys = units.keySet();
+		return keys.toArray(new String[keys.size()]);
 	}
 
 }
