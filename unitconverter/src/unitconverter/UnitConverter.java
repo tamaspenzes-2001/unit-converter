caae794b56c9738a21f58b3916c26750d63d75fb
tamaspenzes-2001 <penzestamas@proton.me>
2025-03-06 19:09:26 +0100

    add Javadoc documentation

--- unitconverter/src/unitconverter/UnitConverter.java
+++ unitconverter/src/unitconverter/UnitConverter.java
@@ -4,8 +4,16 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Set;
 
+/**
+ * A class which can be used to convert between units provided upon instantiation.
+ * Its constructor takes an array of Strings with the format unit:multiplier
+ * (units are listed from largest to smallest, multipliers of units are based on the previous unit).
+ */
 public class UnitConverter{
 	
+	/**
+	 * Store units with multipliers (multipliers of units are based on the largest unit, multiplier of largest unit is always 1).
+	 */
 	private Map<String, Double> units;
 
 	public UnitConverter(String[] units){
@@ -18,10 +26,23 @@ public class UnitConverter{
 		}
 	}
 	
+	/**
+	 * Convert an amount from fromUnit to toUnit
+	 * by dividing amount by the multiplier belonging to fromUnit
+	 * and multiplying the result by the multiplier belonging to toUnit.
+	 * @param amount: the amount to convert
+	 * @param fromUnit: the unit to convert from
+	 * @param toUnit: the unit to convert to
+	 * @return converted amount
+	 */
 	public double convert(double amount, String fromUnit, String toUnit) {
 		return amount / (double) units.get(fromUnit) * units.get(toUnit);
 	}
 	
+	/**
+	 * Get all units.
+	 * @return all units included in a String array
+	 */
 	public String[] getUnits() {
 		Set<String> keys = units.keySet();
 		return keys.toArray(new String[keys.size()]);
